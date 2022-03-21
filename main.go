03b2f9cdab4f02449bef70629f4bced9338ce981
Reddy <kulreddy@deloitte.com>
2022-03-21 20:33:15 +0530

    added divide page handler

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"errors"
 )
 
 const portNumber = ":8080"
@@ -23,10 +24,32 @@ func AddValues(x,y int) int {
 	return x + y
 }
 
+func Divide(w http.ResponseWriter, r *http.Request) {
+	f, err := divideValues(100.0,10.0)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot divide by 0")
+		return
+	}
+
+	fmt.Fprintf(w,fmt.Sprintf("%f divided by %f is %f",100.0,10.0,f))
+
+}
+
+func divideValues(x,y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("Cannot divide by zero")
+		return 0, err
+	}
+	result := x/y
+	return result,nil
+}
+
+
 // main is the main application function
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about",About)
+	http.HandleFunc("/divide",Divide)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s",portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
